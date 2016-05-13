f9a16149732250b9e1d7a827f6c89288db3cf4a5
Kasper Bruus Frank <kasperbfrank@Kaspers-MacBook-Pro.local>
2016-05-13 18:40:51 +0200

    initial commit + node + npm globals setup script.

---
+++ npm-global-installs.sh
@@ -0,0 +1,23 @@
+##### THIS SHELL SCRIPT IS USED TO INSTALL NODE AND GLOBALS #####
+
+#homebrew install (remove comment below if you need to install homebrew.. This is needed in order to install node.)
+#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#node install (remove comment below if you need to install node..)
+#brew install node
+
+#npm global installs
+
+npm install -g bower
+npm install -g grunt-cli
+npm install -g gulp
+npm install -g jade
+npm install -g karma
+npm install -g node-sass
+npm install -g nodemon
+npm install -g phantomjs
+npm install -g protractor
+npm install -g typescript
+npm install -g typings
+npm install -g nvm
+npm install -g angular-cli
